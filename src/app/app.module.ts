af21e6cfc6b8f88d5e12263a67df6e89d7196e06
Ben Jordan <bej9038@rit.edu>
2024-03-10 15:41:09 -0600

    Working on displaying audio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { InputComponent } from './input/input.component';
 import { FormsModule } from '@angular/forms'; // Import FormsModule
 import { HttpClientModule } from '@angular/common/http';
+import { InterfaceComponent } from './interface/interface.component';
+import { AppComponent } from './app.component';
+import { MatCardModule } from "@angular/material/card";
+import {MatInputModule} from "@angular/material/input";
+import {MatSliderModule} from "@angular/material/slider";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatButtonModule} from "@angular/material/button";
+import {MatGridListModule} from "@angular/material/grid-list";
+import { NavbarComponent } from './navbar/navbar.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 @NgModule({
   declarations: [
-    InputComponent
+    AppComponent,
+    InputComponent,
+    InterfaceComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatCardModule,
+    MatInputModule,
+    MatSliderModule,
+    MatButtonToggleModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatToolbarModule
   ],
   providers: [],
-  bootstrap: [InputComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
