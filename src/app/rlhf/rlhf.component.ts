ade80d0a97b407c56631bd5f1ec0201f3b17144a
Ben Jordan <bej9038@rit.edu>
2024-03-20 22:09:29 -0600

    added download menu

---
+++ src/app/rlhf/rlhf.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rlhf',
+  templateUrl: './rlhf.component.html',
+  styleUrls: ['./rlhf.component.css']
+})
+export class RlhfComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
