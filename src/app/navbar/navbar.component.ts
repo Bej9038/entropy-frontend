e227b1632c47903171cb1d0711c52a5a81cfb46f
Ben Jordan <bej9038@rit.edu>
2024-03-29 07:44:04 -0600

    angular update

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FirebaseService} from "../firebase.service";
 
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
