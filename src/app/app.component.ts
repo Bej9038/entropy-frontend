6ba8d9594077210f6f8d2f356ecf3689114ec5f0
Ben Jordan <bej9038@rit.edu>
2024-04-03 20:45:26 -0600

    added a cover element to receive clicks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -52,6 +52,7 @@ export class AppComponent {
   }
 
   ngOnInit(): void {
+
     // this.startPeriodicRequest()
   }
 
--- src/app/wavebox/wavebox.component.ts
+++ src/app/wavebox/wavebox.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, HostListener, Inject, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, HostListener, Inject, Input, OnInit, Renderer2, ViewChild} from '@angular/core';
 import {AudioService} from "../services/audio.service";
 import {ProgressBarMode} from "@angular/material/progress-bar";
 import WaveSurfer from "wavesurfer.js";
@@ -18,6 +18,7 @@ export class WaveboxComponent implements OnInit {
   wavesurfer: WaveSurfer | undefined;
   focused = false;
   progressBarMode: ProgressBarMode = "indeterminate";
+  grabbing = false;
 
   rootStyle = getComputedStyle(this.document.documentElement);
   grey = this.rootStyle.getPropertyValue("--translucent-grey-2").trim()
@@ -27,12 +28,37 @@ export class WaveboxComponent implements OnInit {
 
   constructor(public audioService: AudioService,
               public stateService: StateService,
-              @Inject(DOCUMENT) private document: Document) {}
+              @Inject(DOCUMENT) private document: Document,
+              private renderer: Renderer2,) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+
+  }
+
+  ngAfterViewInit()
+  {
+
+  }
 
   onFocus() {
     this.focused = true;
+  }
+
+  onMouseDown()
+  {
+    // this.grabbing = true;
+    console.log("hi")
+    // let style = document.createElement( 'style' )
+    // style.innerHTML = ':host .progress { pointer-events: auto !important }'
+    // let innerDiv = this.waveform.nativeElement.childNodes[0]
+    // console.log(innerDiv.shadowRoot.childNodes[1])
+    // innerDiv.shadowRoot.childNodes[1].innerHTML = ":host {min-width: 1px} :host audio {display: block;width: 100%;}:host .scroll {overflow-x: auto;overflow-y: hidden;width: 100%;position: relative;}:host .noScrollbar {scrollbar-color: transparent;scrollbar-width: none;}:host .noScrollbar::-webkit-scrollbar {display: none; -webkit-appearance: none;}:host .wrapper {position: relative;overflow: visible;z-index: 0;}:host .canvases {min-height: 72px;}:host .canvases > div {position: relative;}:host canvas {display: block;position: absolute;top: 0;image-rendering: pixelated;}:host .progress {position: absolute;z-index: 0;top: 0;left: 0;width: 0;height: 100%;overflow: hidden;}:host .progress > div {position: relative;}:host .cursor {position: absolute;z-index: 0;top: 0;left: 0;height: 100%;border-radius: 2px;}"
+    // innerDiv.shadowRoot.childNodes[1].innerHTML = ":host {z-index: -1 !important; pointer-events: auto!important;}:host .scroll {overflow: hidden; z-index: -1 !important;}"
+  }
+
+  onMouseUp()
+  {
+    this.grabbing = false;
   }
 
   unFocus() {
@@ -44,6 +70,11 @@ export class WaveboxComponent implements OnInit {
 
   initialize() {
     this.wavesurfer?.destroy()
+  }
+
+  selectWavebox()
+  {
+
   }
 
   initWaveSurfer(src: any, debug:boolean)
